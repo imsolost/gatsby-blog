28b67ec851ef22f06abaadb4cea4e7590d8569e9
imsolost <imsolost@gmail.com>
2019-02-12 12:27:01 -0800

    kind of have the navbar working

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default ({ data }) => {
                 text-decoration: none;
                 color: inherit;
               `
-}
+              }
             >
               <h3
                 css={css`
